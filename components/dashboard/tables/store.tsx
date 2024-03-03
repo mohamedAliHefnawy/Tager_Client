12caf6fbf90be5bdcb8ed615dae0c72ee36f7e10
mohamedAliHefnawy <140818071+mohamedAliHefnawy@users.noreply.github.com>
2024-03-02 23:27:10 -0800

    main

--- components/dashboard/tables/store.tsx
+++ components/dashboard/tables/store.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import linkServer from "@/linkServer";
 import { toast } from "react-toastify";
-import ReactAudioPlayer from "react-audio-player";
 
 //nextUi
 import {
