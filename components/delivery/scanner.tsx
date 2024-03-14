1da101082b32fea95c924dc028a940777108b696
mohamedAliHefnawy <140818071+mohamedAliHefnawy@users.noreply.github.com>
2024-03-14 04:29:06 -0700

    main

--- components/delivery/scanner.tsx
+++ components/delivery/scanner.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, Component } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import Swal from "sweetalert2";
 import linkServer from "@/linkServer";
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import { Switch } from "@nextui-org/react";
+import QrReader from "react-qr-reader";
 
 export default function QRScanner({ name }: { name: string }) {
+  const nameDelivery = localStorage.getItem("nameDelivery");
   const [scanResult, setScanResult] = useState<string | null>(null);
   const [isScannerActive, setScannerActive] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const [data, setData] = useState("No result");
 
   const router = useRouter();
 
@@ -26,17 +30,17 @@ export default function QRScanner({ name }: { name: string }) {
     try {
       setIsLoading(true);
       const data = {
-        deliveryName: name,
+        deliveryName: nameDelivery,
         idOrder: scannedResult,
       };
       const response = await axios.post(
         `${linkServer.link}scanner/addOrderWithDelivery`,
         data
       );
-
       if (response.data === "yes") {
         router.push("/delivery/orders");
-      } else {
+      }
+      if (response.data === "idOrder already exists") {
         Swal.fire({
           icon: "warning",
           title: "هذه الطلبية موجودة بالفعل",
@@ -52,6 +56,16 @@ export default function QRScanner({ name }: { name: string }) {
     }
   };
 
+  const handleScannerError = (error: any) => {
+    console.log(error?.message);
+  };
+
+  const handleScannerScan = (result: any) => {
+    if (result) {
+      handleScannerResult(result);
+    }
+  };
+
   useEffect(() => {
     if (scanResult) {
       handleScannerResult(scanResult);
@@ -60,26 +74,32 @@ export default function QRScanner({ name }: { name: string }) {
 
   return (
     <>
-      {isLoading ? (
+      {/* {isLoading ? (
         <p className="text-center">يرجى الانتظار</p>
       ) : (
-        <>
-          <Switch
-            className="rotate-90"
-            onClick={toggleScanner}
-            defaultSelected
-            color="warning"
-          />
-          {isScannerActive && (
-            <QrScanner
-              audio
-              tracker
-              onResult={handleScannerResult}
-              onError={(error) => console.log(error?.message)}
-            />
-          )}
-        </>
+        <> */}
+      <Switch
+        className="rotate-90"
+        onClick={toggleScanner}
+        defaultSelected
+        color="warning"
+      />
+      {isScannerActive && (
+        <QrReader
+          delay={10000}
+          onError={handleScannerError}
+          onScan={handleScannerScan}
+        />
+        // <QrScanner
+        //   audio
+        //   tracker
+        //   scanDelay={500}
+        //   onResult={handleScannerResult}
+        //   onError={(error) => console.log(error?.message)}
+        // />
       )}
+      {/* </>
+      )} */}
     </>
   );
 }
